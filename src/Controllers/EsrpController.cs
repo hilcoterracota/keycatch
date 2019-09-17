e65da033bf2c4b088753a2c780d47654dc7f34ed
Nestor Martinez <nestor-mh@outlook.com>
2019-09-17 14:32:27 -0500

    Esrp controller

--- src/Controllers/EsrpController.cs
+++ src/Controllers/EsrpController.cs
@@ -1,6 +1,11 @@
 
 using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Sampekey.Interface;
 using Sampekey.Model.Configuration.Module;
 
@@ -10,46 +15,82 @@ namespace keycatch.Controllers
     [ApiController]
     public class EsrController : ControllerBase
     {
-        private readonly IEsrp kingdomCastleRolePermission;
+        private readonly IEsrp esrp;
+        private readonly ILogger logger;
 
-        public EsrController(IEsrp _kingdomCastleRolePermission)
+        public EsrController(IEsrp _esrp,ILogger<EsrController> _logger)
         {
-            kingdomCastleRolePermission = _kingdomCastleRolePermission;
+            esrp = _esrp;
+            logger = _logger;
         }
 
         [HttpGet]
         [Route("V1")]
-        public ActionResult<IEnumerable<KingdomCastleRolePermission>> GetAllKingdomCastleRolePermissions()
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(IEnumerable<KingdomCastleRolePermission>), (int)HttpStatusCode.OK)]
+        public ActionResult<IEnumerable<KingdomCastleRolePermission>> GetAllKingdomCastleRolePermissionsAsync()
         {
-            return Ok(kingdomCastleRolePermission.GetAllKingdomCastleRolePermissions());
+            Task<IEnumerable<KingdomCastleRolePermission>> data = esrp.GetAllKingdomCastleRolePermissions();
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else if (data.Result == null) return NoContent();
+            else return Ok(data.Result);
         }
 
         [HttpGet]
         [Route("V1/{id}")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(Kingdom), (int)HttpStatusCode.OK)]
         public ActionResult<KingdomCastleRolePermission> FindKingdomCastleRolePermissionById(string id)
         {
-            return Ok(kingdomCastleRolePermission.FindKingdomCastleRolePermissionById(id));
+            Task<KingdomCastleRolePermission> data = esrp.FindKingdomCastleRolePermissionById(id);
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else if (data.Result == null) return NoContent();
+            else return Ok(data.Result);
         }
 
         [HttpPost]
         [Route("V1")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(Kingdom), (int)HttpStatusCode.OK)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public ActionResult<KingdomCastleRolePermission> AddKingdomCastleRolePermission([FromBody] KingdomCastleRolePermission value)
         {
-            return Ok(kingdomCastleRolePermission.AddKingdomCastleRolePermission(value));
+            Task<KingdomCastleRolePermission> data = esrp.AddKingdomCastleRolePermission(value);
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else if (data.Result == null) return NoContent();
+            else return Ok(data.Result);
         }
 
         [HttpPut]
         [Route("V1")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(Kingdom), (int)HttpStatusCode.OK)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public ActionResult<KingdomCastleRolePermission> UpdateKingdomCastleRolePermission([FromBody] KingdomCastleRolePermission value)
         {
-            return Ok(kingdomCastleRolePermission.UpdateKingdomCastleRolePermission(value));
+            Task<KingdomCastleRolePermission> data = esrp.UpdateKingdomCastleRolePermission(value);
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else if (data.Result == null) return NoContent();
+            else return Ok(data.Result);
         }
 
         [HttpDelete]
         [Route("V1")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public ActionResult<bool> DeleteKingdomCastleRolePermission([FromBody] KingdomCastleRolePermission value)
         {
-            return Ok(kingdomCastleRolePermission.DeleteKingdomCastleRolePermission(value));
+            Task<bool> data = esrp.DeleteKingdomCastleRolePermission(value);
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else return Ok(data.Result);
         }
 
     }
