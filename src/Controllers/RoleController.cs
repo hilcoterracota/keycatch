7e1df56dc39b5f18d35cfe1e7edb12011d7cb159
Nestor Martinez <nestor-mh@outlook.com>
2019-09-20 11:39:33 -0500

    pure

--- src/Controllers/RoleController.cs
+++ src/Controllers/RoleController.cs
@@ -1,6 +1,11 @@
 
+using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Sampekey.Interface;
 using Sampekey.Model.Identity;
 
@@ -10,65 +15,82 @@ namespace keycatch.Controllers
     [ApiController]
     public class RoleController : ControllerBase
     {
-        private readonly ISystemAlert systemAlertRepo;
-        private readonly IRole roleRepo;
-        public RoleController(
-            ISystemAlert _systemAlertRepo,
-            IRole _roleRepo
-        )
+        private readonly IRole role;
+        private readonly ILogger logger;
+        public RoleController(IRole _role, ILogger<RoleController> _logger)
         {
-            systemAlertRepo = _systemAlertRepo;
-            roleRepo = _roleRepo;
+            role = _role;
+            logger = _logger;
         }
 
         [HttpGet]
         [Route("V1")]
-        public async Task<ActionResult<Role>> GetRoles()
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(IEnumerable<Role>), (int)HttpStatusCode.OK)]
+        public ActionResult<Task<IEnumerable<Role>>> GetAllRoles()
         {
-            return Ok(await roleRepo.GetRoles());
+            Task<IEnumerable<Role>> data = role.GetAllRoles();
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else if (data.Result == null) return NoContent();
+            else return Ok(data.Result);
         }
 
-
-        [HttpPost]
-        [Route("V1/CreateRole")]
-        public async Task<ActionResult<Role>> CreateRole([FromBody] Role role)
+        [HttpGet]
+        [Route("V1/{id}")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(Role), (int)HttpStatusCode.OK)]
+        public ActionResult<Role> FindRoleById(string id)
         {
-            if (ModelState.IsValid)
-            {
-                if ((await roleRepo.CreateRole(role)).Succeeded)
-                {
-                    return Ok((await roleRepo.FindRoleByName(role.Name)));
-                }
-                else
-                {
-                    return ValidationProblem();
-                }
-            }
-            else
-            {
-                return Unauthorized(systemAlertRepo.GetUnauthorizedMenssageFromActiveDirectory());
-            }
+            Task<Role> data = role.FindRoleById(id);
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else if (data.Result == null) return NoContent();
+            else return Ok(data.Result);
         }
 
         [HttpPost]
-        [Route("V1/AddClaimToRole")]
-        public async Task<ActionResult<Role>> AddClaimToRole([FromBody] RoleClaim roleClaim)
+        [Route("V1")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(Role), (int)HttpStatusCode.OK)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<Role> AddRole([FromBody] Role value)
         {
-            if (ModelState.IsValid)
-            {
-                if ((await roleRepo.AddClaimAsyncToRole(await roleRepo.FindRoleById(roleClaim.RoleId), roleClaim.ToClaim())).Succeeded)
-                {
-                    return Ok();
-                }
-                else
-                {
-                    return BadRequest();
-                }
-            }
-            else
-            {
-                return ValidationProblem();
-            }
+            Task<Role> data = role.AddRole(value);
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else if (data.Result == null) return NoContent();
+            else return Ok(data.Result);
         }
+
+        [HttpPut]
+        [Route("V1")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(Role), (int)HttpStatusCode.OK)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<Role> UpdateRole([FromBody] Role value)
+        {
+            Task<Role> data = role.UpdateRole(value);
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else if (data.Result == null) return NoContent();
+            else return Ok(data.Result);
+        }
+
+        [HttpDelete]
+        [Route("V1")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<bool> DeleteRole([FromBody] Role value)
+        {
+            Task<bool> data = role.DeleteRole(value);
+            if (data.IsCanceled) return BadRequest(data.Exception);
+            else return Ok(data.Result);
+        }
+
     }
 }
